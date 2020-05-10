cb31a625fa2c9f927756131d10ed776c77f8b361
wsrtka <witold.serwatka@pm.me>
2020-05-10 19:48:47 +0200

    initial commit

---
+++ src/main/java/DBook/project/app/DBookApplication.java
@@ -0,0 +1,13 @@
+package DBook.project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DBook/project/app/DBookApplicationTests.java
@@ -0,0 +1,13 @@
+package DBook.project.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

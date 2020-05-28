483115628069fa041f31c3791515977ff57e8ca2
wsrtka <witold.serwatka@pm.me>
2020-05-28 17:06:10 +0200

    added toString method

--- src/main/java/DBook/project/app/book/BookType.java
+++ src/main/java/DBook/project/app/book/BookType.java
@@ -1,5 +1,20 @@
 package DBook.project.app.book;
 
 public enum BookType {
-    TEXT_BOOK, EXERCISE_BOOK, COMPENDIUM, OTHER
+
+    TEXT_BOOK, EXERCISE_BOOK, COMPENDIUM, OTHER;
+
+    private String toString;
+
+    static {
+        TEXT_BOOK.toString = "Text book";
+        EXERCISE_BOOK.toString = "Exercise book";
+        COMPENDIUM.toString = "Compendium";
+        OTHER.toString = "Other";
+    }
+
+    @Override
+    public String toString(){
+        return this.toString;
+    }
 }

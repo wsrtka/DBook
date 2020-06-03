c318845798be343e5562ade400de29264df0182e
wsrtka <witold.serwatka@pm.me>
2020-06-03 14:37:22 +0200

    formatted code, added transactions to methods

--- src/main/java/DBook/project/app/users/Employee.java
+++ src/main/java/DBook/project/app/users/Employee.java
@@ -20,9 +20,9 @@ public class Employee extends User{
 
     }
 
-    public void listSomeoneInvoices(Integer userID){
+    public void listSomeoneInvoices(Integer userID, Transaction tx){
 
-        ArrayList<Integer> invoicesIDList = new ArrayList(this.dBookApplication.getUserArrayList().get(userID).getUsersInvoices().entrySet());
+        ArrayList<Integer> invoicesIDList = new ArrayList(this.dBookApplication.getUserArrayList().get(userID).getUsersInvoices(tx).entrySet());
 
         for (Integer invoiceID: invoicesIDList) {
             System.out.println(invoiceID+";");
@@ -30,62 +30,85 @@ public class Employee extends User{
 
     }
 
-    public void listSomeoneOffers(Integer userID){//wylistowuje oferty danego uzytkownika
-        ArrayList<Integer> invoicesIDList = new ArrayList(this.dBookApplication.getUserArrayList().get(userID).getUsersOffers().entrySet());
+    public void listSomeoneOffers(Integer userID, Transaction tx){
+
+        ArrayList<Integer> invoicesIDList = new ArrayList(this.dBookApplication.getUserArrayList().get(userID).getUsersOffers(tx).entrySet());
+
         for (Integer invoiceID: invoicesIDList) {
             System.out.println(invoiceID+";");
         }
+
     }
 
-    public void listAllUsers(){//wylistowuje wszystkich uzytkownikow
+    public void listAllUsers(){
+
         for(User user:this.dBookApplication.getUserArrayList()){
             System.out.println(user.toString()+";");
         }
+
     }
 
-    public Double calculateInvoice(Integer userID, Transaction txt){ // ma zwrocic ile ma dany uzytkownik ma zaplacic
-        ArrayList<Integer> invoicesIDList = new ArrayList(this.dBookApplication.getUserArrayList().get(userID).getUsersOffers().entrySet());
+    public Double calculateInvoice(Integer userID, Transaction tx){
+
+        ArrayList<Integer> invoicesIDList = new ArrayList(this.dBookApplication.getUserArrayList().get(userID).getUsersOffers(tx).entrySet());
         Double result = new Double(0.0);
+
         for (Integer invoiceID: invoicesIDList) {
-            result += this.dBookApplication.getUserArrayList().get(userID).calculateInvoice(invoiceID, txt);
+            result += this.dBookApplication.getUserArrayList().get(userID).calculateInvoice(invoiceID, tx);
         }
+
         return result;
+
     }
 
     public Double calculateOffer(Integer userID, Transaction tx){
+
         User user = dBookApplication.getUserArrayList().get(userID);
         Money money = new Money();
-        user.getUsersOffers().forEach((k, v) ->money.add(v.calculateOfferRevenue()));
+
+        user.getUsersOffers(tx).forEach((k, v) ->money.add(v.calculateOfferRevenue()));
+
         return money.getValue();
+
     }
 
-    public ArrayList<Book> listBooksToReturn(Integer userID){ //listuje ksiązki, które mamy zwrócić klientowi
+    public ArrayList<Book> listBooksToReturn(Integer userID, Transaction tx){
+
         ArrayList<Book> booksToReturn = new ArrayList<>();
         User user = dBookApplication.getUserArrayList().get(userID);
 
-        user.getUsersOffers().forEach((k, v) -> booksToReturn.addAll(v.getUnsoldBooks()));
+        user.getUsersOffers(tx).forEach((k, v) -> booksToReturn.addAll(v.getUnsoldBooks()));
 
         return booksToReturn;
     }
 
-    public ArrayList<Book> listInvoiceBooks(Integer invoiceID, Integer userID){ //wypisuje ksiazki, ktore maja byc przyniesione dla klienta
+    public ArrayList<Book> listInvoiceBooks(Integer invoiceID, Integer userID, Transaction tx){
+
         ArrayList<Book> booksToBeBrought = new ArrayList<>();
-        Invoice invoice = dBookApplication.getUserArrayList().get(userID).getUsersInvoices().get(invoiceID);
+        Invoice invoice = dBookApplication.getUserArrayList().get(userID).getUsersInvoices(tx).get(invoiceID);
 
         booksToBeBrought.addAll(invoice.getInvoiceBooks().values());
+
         return booksToBeBrought;
+
     }
 
-    public ArrayList<Book> listOrderBooks(Integer orderID, Integer userID){// wypisuje ksiazki, ktore maja byc przyniesione przez klienta
+    public ArrayList<Book> listOrderBooks(Integer orderID, Integer userID, Transaction tx){
+
         ArrayList<Book> booksOffer = new ArrayList<>();
 
-        Offer offer = dBookApplication.getUserArrayList().get(userID).getUsersOffers().get(orderID);
+        Offer offer = dBookApplication.getUserArrayList().get(userID).getUsersOffers(tx).get(orderID);
         booksOffer.addAll(offer.getBooks().values());
+
         return booksOffer;
+
     }
-    public void acceptInvoice(Integer invoiceID, ArrayList<Book> acceptedBooks, Integer userID, Transaction tx){// akceptuje zamowienie (zostalo zaplacone) i od razu usuwa te ksiazki z zamowienia, ktore nie zostaly kupione na miejscu
-            Invoice invoice = dBookApplication.getUserArrayList().get(userID).getUsersInvoices().get(invoiceID);
+
+    public void acceptInvoice(Integer invoiceID, ArrayList<Book> acceptedBooks, Integer userID, Transaction tx){
+
+            Invoice invoice = dBookApplication.getUserArrayList().get(userID).getUsersInvoices(tx).get(invoiceID);
             invoice.acceptInvoice();
+
             for(Book book:acceptedBooks){
                 book.claimBook();
                 book.update(tx);
@@ -97,11 +120,14 @@ public class Employee extends User{
                     v.update(tx);
                 }
             });
+
     }
 
-    public void acceptOffer(Integer offerID, ArrayList<Book> acceptedBooks, Integer userID, Transaction tx){//akceptuje zamowienie (ksiazki zostaly przyniesione) i od razu usuwa te ksiazki z zamowienia, ktore nie zostaly przyjete/przyniesione
-        Offer offer = dBookApplication.getUserArrayList().get(userID).getUsersOffers().get(offerID);
+    public void acceptOffer(Integer offerID, ArrayList<Book> acceptedBooks, Integer userID, Transaction tx){
+
+        Offer offer = dBookApplication.getUserArrayList().get(userID).getUsersOffers(tx).get(offerID);
         offer.acceptOffer();
+
         for(Book book:acceptedBooks){
             book.claimBook();
             book.update(tx);
@@ -110,30 +136,36 @@ public class Employee extends User{
         offer.getOfferBooks().forEach((k, v)->{
             if(!acceptedBooks.contains(v)){
                 v.removeFromDB(tx);
-                offer.getBooks().remove(v); // nie jestem pewien czy zadziała to wywołanie poprawnie
+//                uwaga: może nie działać poprawnie
+                offer.getBooks().remove(v);
             }
         });
     }
 
     public void deleteUnacceptedOffers(Transaction tx){
+
         for(User user : dBookApplication.getUserArrayList()){
-            user.getUsersOffers().forEach((k, v)->{
+            user.getUsersOffers(tx).forEach((k, v)->{
                 if(v.isAccepted()){
                     v.removeFromDB(tx);
-                    user.getUsersOffers().remove(v); // nie jestem pewien czy zadziała
+//                    uwaga: może nie działać
+                    user.getUsersOffers(tx).remove(v);
+                }
+            });
+        }
+
+    }
+
+    public void deleteUnacceptedInvoices(Transaction tx){
+        for(User user : dBookApplication.getUserArrayList()){
+            user.getUsersInvoices(tx).forEach((k, v)->{
+                if(v.isAccepted()){
+                    v.removeFromDB(tx);
+//                    uwaga: może nie działać
+                    user.getUsersInvoices(tx).remove(v);
                 }
             });
         }
     }
 
-    public void deleteUnacceptedInvoices(Transaction tx){
-        for(User user : dBookApplication.getUserArrayList()){
-            user.getUsersInvoices().forEach((k, v)->{
-                if(v.isAccepted()){
-                    v.removeFromDB(tx);
-                    user.getUsersInvoices().remove(v); // nie jestem pewien czy zadziała
-                }
-            });
-        }
-    }
 }

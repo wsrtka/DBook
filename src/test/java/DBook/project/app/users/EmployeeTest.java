8ff150f748a9ab9dd2022e0fe7adaa102af12147
wsrtka <witold.serwatka@pm.me>
2020-06-03 14:51:59 +0200

    verified addToDB function for Employee

--- src/test/java/DBook/project/app/users/EmployeeTest.java
+++ src/test/java/DBook/project/app/users/EmployeeTest.java
@@ -1,4 +1,23 @@
 package DBook.project.app.users;
 
+import DBook.project.app.DBookApplication;
+import org.junit.jupiter.api.Test;
+import org.neo4j.driver.AccessMode;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.SessionConfig;
+
 public class EmployeeTest {
+
+    private Employee employee = new Employee(new DBookApplication(), "Piotr Dariusz", "Świderski", "pds@falelo.pl");
+    private DBookApplication dbApp = new DBookApplication();
+
+    @Test
+    public void addToDBTest(){
+
+        try(Session s = dbApp.getDriver().session(SessionConfig.builder().withDefaultAccessMode(AccessMode.WRITE).build())) {
+            s.writeTransaction(employee::addToDB);
+        }
+
+    }
+
 }
